af8618999d9f02d9b2fe40935ec0a8923fd31292
Stephanie Patton <31425878+StephanieRP@users.noreply.github.com>
2019-03-18 17:55:42 -0500

    added click event

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,18 +1,29 @@
 import React from "react";
 import "./Footer.css";
 
-const countUp = () => {
-  let num = 0;
-  return (num += num);
+const toggleNum = e => {
+  let num = document.querySelectorAll(".number");
+  num.forEach(number => {
+    console.log(number);
+    let count = 1;
+    return (number.innerHTML = count++);
+  });
 };
 
 const Footer = () => {
   return (
     <div className="icon-content">
-      <i class="far fa-comment" />
-      <i class="fas fa-sync-alt" />
-      <i class="far fa-heart" />
-      <i class="far fa-envelope" />
+      <div>
+        <i className="far fa-comment" onClick={toggleNum} />
+        <span className="number" />
+      </div>
+      <i className="fas fa-sync-alt" />
+      <div>
+        <i className="far fa-heart" onClick={toggleNum} />
+        <span className="number" />
+      </div>
+
+      <i className="far fa-envelope" />
     </div>
   );
 };
