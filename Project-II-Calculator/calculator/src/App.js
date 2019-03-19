5c37c681b2800b8f6e28855d031b0a453261fcca
Stephanie Patton <31425878+StephanieRP@users.noreply.github.com>
2019-03-19 16:50:53 -0500

    created App class

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -6,24 +6,45 @@ import NumberButton from "./components/ButtonComponents/NumberButton";
 
 const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3];
 
-const App = props => {
-  console.log(props);
-  return (
-    <div className="main-container">
-      <div className="display-container">
-        <CalculatorDisplay />
-      </div>
-      <div className="button-container">
-        <div className="numbers-container">
-          <NumberButton calNumbers={numbers} />
+class App extends React.Component {
+  render() {
+    return (
+      <div className="main-container">
+        <div className="display-container">
+          <CalculatorDisplay />
         </div>
+        <div className="button-container">
+          <div className="numbers-container">
+            <NumberButton calNumbers={numbers} />
+          </div>
 
-        <div className="action-container">
-          <ActionButton />
+          <div className="action-container">
+            <ActionButton />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+// const App = props => {
+//   console.log(props);
+//   return (
+//     <div className="main-container">
+//       <div className="display-container">
+//         <CalculatorDisplay />
+//       </div>
+//       <div className="button-container">
+//         <div className="numbers-container">
+//           <NumberButton calNumbers={numbers} />
+//         </div>
+
+//         <div className="action-container">
+//           <ActionButton />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default App;
