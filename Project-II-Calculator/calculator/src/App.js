09fdfc22a76b735f6de8287bd95b139454be76fe
Stephanie Patton <31425878+StephanieRP@users.noreply.github.com>
2019-03-19 22:47:05 -0500

    changed array to object

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -4,7 +4,17 @@ import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay"
 import ActionButton from "./components/ButtonComponents/ActionButton";
 import NumberButton from "./components/ButtonComponents/NumberButton";
 
-const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3];
+const numbers = [
+  { num: 7, position: 1 },
+  { num: 8, position: 2 },
+  { num: 9, position: 3 },
+  { num: 4, position: 4 },
+  { num: 5, position: 5 },
+  { num: 6, position: 6 },
+  { num: 1, position: 7 },
+  { num: 2, position: 8 },
+  { num: 3, position: 9 }
+];
 
 class App extends React.Component {
   constructor() {
@@ -12,7 +22,16 @@ class App extends React.Component {
     this.state = {
       total: 0
     };
+    this.updateDisplay = this.updateDisplay.bind(this);
   }
+  updateDisplay() {
+    this.setState(prevSate => {
+      return {
+        total: (prevSate.total += { numbers })
+      };
+    });
+  }
+
   render() {
     return (
       <div className="main-container">
@@ -20,8 +39,15 @@ class App extends React.Component {
           <CalculatorDisplay display={this.state.total} />
         </div>
         <div className="button-container">
-          <div className="numbers-container">
-            <NumberButton calNumbers={numbers} />
+          <div className="row">
+            <div className="numbers-button special">clear</div>
+
+            <div className="numbers-container">
+              {numbers.map(num => (
+                <NumberButton calNumbers={num} onClick={this.updateDisplay} />
+              ))}
+            </div>
+            <div className="numbers-button special">0</div>
           </div>
 
           <div className="action-container">
